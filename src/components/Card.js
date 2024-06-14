d610290c223eeab371949a779d9837ff6dee11fc
Jessy-Jones <93819297+Jessy-Jones@users.noreply.github.com>
2024-06-14 15:57:32 +0000

    Introducing Props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Star from "../images/star.png"
+
 
 export default function Card(props){
     return(
         <div className="card">
           
             <img src={`../images/${props.img}`} alt="Katie Zaferes" className="movie-image"/>
-            <p> <img src={Star} alt="Rating" className="star"/> {props.rating}<span>({props.reviewCount}).{props.country}</span></p>
+            <p> <img src="/images/star.png" alt="Rating" className="star"/> {props.rating}<span>({props.reviewCount}).{props.country}</span></p>
             <p>{props.title}</p>
             <p>From ${props.price} <span>/person</span></p>
        
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from "react"
-import AirBnB from "../images/airbnb_logo.png"
 
 
 export default function NavBar(){
     return(
         <nav className="logo--container">
-        <img src={AirBnB} className="logo" alt="Airbnb logo"/>
+        <img src= "/images/airbnb_logo.png" className="logo" alt="Airbnb logo"/>
         </nav>
     )
 }
